b016ea5618dd203434eac8ed406917712c94fe5c
ch-shubham <shubham128166@gmail.com>
2019-07-06 18:25:50 +0530

    fetch post comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ function RenderDish({dish}) {
             );
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if(comments !== null)
         return(
             <div className="col-12 col-md-5 m-1">
@@ -43,7 +43,7 @@ function RenderDish({dish}) {
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     }
@@ -67,7 +67,7 @@ function RenderDish({dish}) {
 
           RenderComments(values) {
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
             // console.log('Current State is: ' + JSON.stringify(values));
             // alert('Current State is: ' + JSON.stringify(values));
     
@@ -176,7 +176,7 @@ function RenderDish({dish}) {
                     <div className="row">
                             <RenderDish dish={props.dish} />
                             <RenderComments comments={props.comments}
-                                addComment={props.addComment}
+                                postComment={props.postComment}
                                 dishId={props.dish.id}
                             />
                             
